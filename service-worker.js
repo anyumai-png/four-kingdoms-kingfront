89d8940a7d66e0d5043f26c49f202e6c1df991dc
anyumai-png <andyyum.ai@gmail.com>
2026-09-15 08:12:26 +0800

    Bust stale Pages loader cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="kingfront-v0.96.0";
+const CACHE="kingfront-v0.96.0-pagesfix1";
 const SHELL=["./","./index.html","./manifest.webmanifest","./assets/icon-192.png","./assets/icon-512.png","./assets/apple-touch-icon.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith("kingfront-v")&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
